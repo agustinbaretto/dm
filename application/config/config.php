c138bb469430fdfa41b9b4a49e748b550d7a765c
agustinbaretto <agustin@Agustins-MacBook-Air.local>
2014-12-22 20:13:38 -0300

    config fb

--- application/config/config.php
+++ application/config/config.php
@@ -356,8 +356,8 @@ $config['rewrite_short_tags'] = FALSE;
 |
 */
 $config['proxy_ips'] = '';
-$config['facebook']['api_id'] = '755910007779705';
-$config['facebook']['app_secret'] = '2f9c0eb2620af786e62926c3e4249ae6';
+$config['facebook']['api_id'] = '163110543856721';
+$config['facebook']['app_secret'] = 'b62e18454dea10acc6f70175ad6e6414';
 $config['facebook']['redirect_url'] = 'http://gustame.com/dm/index.php/main/';
 $config['facebook']['permissions'] = array('email',
 		'user_friends',
--- application/config/facebook.php
+++
@@ -1,4 +0,0 @@
-<?php
-$config['facebook_app_id']          = '755910007779705';
-$config['facebook_api_secret']      = '2f9c0eb2620af786e62926c3e4249ae6';
-$config['facebook_default_scope']   = 'email,user_friends';
