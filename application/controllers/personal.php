cab28c768336f48da4f8bf62bb89240d86674d64
agustinbaretto <agustin@Agustins-MacBook-Air.local>
2015-01-04 20:54:32 -0300

    lala2

---
+++ application/controllers/personal.php
@@ -0,0 +1,36 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Personal extends CI_Controller {
+	
+	public function index()
+	{
+		$this->load->view('templates/headerPersonal');
+		$this->load->view('pages/about');
+		$this->load->view('templates/footer');
+	}
+	
+	public function blog()
+	{
+		$this->load->view('templates/headerPersonal');
+		$this->load->view('pages/blog');
+		$this->load->view('templates/footer');
+	}
+	
+	public function contact()
+	{
+		$this->load->view('templates/headerPersonal');
+		$this->load->view('pages/contact');
+		$this->load->view('templates/footer');
+	}
+	
+	public function test()
+	{
+		//$this->load->driver('cache');
+		//session_start();
+		//$_SESSION["hola"] = "chau";
+		echo $_SESSION["hola"];
+	}
+}
+
+/* End of file personal.php */
+/* Location: ./application/controllers/personal.php */
