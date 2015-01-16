fa18516c8cbc279113f8c8e3d352b6244f4d808f
agustinbaretto <agustin@Agustins-MacBook-Air.local>
2015-01-15 20:41:06 -0500

    about

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -4,7 +4,11 @@
 				<div class="col-lg-8 col-lg-offset-2 centered">
 					<img src="assets/img/user.png" alt="Stanley">
 					<h1>About Gustame</h1>
-					<p>Gustame is a </p>
+					<p>Gustame is an app I developed as a class project for the Data Mining course I took during the fall of 2014 at University of 
+					Rochester.</p>
+					<p>Christmas was coming and I wanted to give a book as a gift to a friend and so I faced the problem: what books would this friend like? 
+					I had learnt some stuff about recommendation systems so then, I decided to use the Slope One algorithm to develop an app that could 
+					address this problem by making use of information available in social networks about friends interests.</p>
 					
 				</div><!-- /col-lg-8 -->
 			</div><!-- /row -->
@@ -39,8 +43,15 @@
 		
 		<div class="row mt">
 			<div class="col-lg-6">
-				<h4>THE THINKING</h4>
-				<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.</p>
+				<h4>HOW DOES IT WORK</h4>
+				<p>By signing in to the site using their Facebook account, users are able to see a list of their friends and for each of those friends, 
+				get recommendations based on the Facebook Pages related to books they already liked. Sounds tricky huh? Then I should not mention that 
+				it uses an open knowledge database (Freebase) to disambiguate redundant entities and that it uses a Slope One algorithm based on your 
+				closed graph of contacts to do the recommendation… but I will give you an example so you can figure out how it works:,/p>
+				<p>Your friend Albert has liked the Facebook the pages “The Hobbit - the book” and “Alice in Wonderland”, so we might assume he has read 
+				those two books.
+Your other friends Bob and Claire also liked those two pages and they also liked “The Narnia Chronicles Book”, which Albert hasn’t. This means they have similar taste to Albert and that they have read a book Albert might enjoy reading too.
+You get a recommendation to give Albert the book “Narnia Chronicles” as a gift.</p>
 			</div><!-- /colg-lg-6 -->
 			
 			<div class="col-lg-6">
--- application/views/pages/contactSuccess.php
+++ application/views/pages/contactSuccess.php
@@ -1,6 +1,6 @@
 	<!-- +++++ Contact Section +++++ -->
 	
-	<div class="container">
+	<div class="container pt">
 		<div class="row mt">
 			<div class="col-lg-6 col-lg-offset-3 centered">
 				<h3>THANKS...</h3>
@@ -8,21 +8,4 @@
 				<p>Keep Calm,<br />and I will contact you back!</p>
 			</div>
 		</div>
-		<div class="row mt">	
-			<div class="col-lg-8 col-lg-offset-2">
-				<form role="form" method="post" accept-charset="utf-8" action="<?= site_url('main/submit_contact') ?>">
-				  <div class="form-group">
-				    <input type="name" class="form-control" id="NameInputEmail1" name="name" placeholder="Your Name">
-				    <br>
-				  </div>
-				  <div class="form-group">
-				    <input type="email" class="form-control" id="exampleInputEmail1" name="email" placeholder="Enter email">
-				    <br>
-				  </div>
-				  <textarea class="form-control" rows="6" name="content" placeholder="Enter your text here"></textarea>
-				    <br>
-				  <button type="submit" class="btn btn-success">SUBMIT</button>
-				</form>    			
-			</div>
-		</div><!-- /row -->
 	</div><!-- /container -->
