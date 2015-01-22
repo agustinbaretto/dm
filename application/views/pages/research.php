ab62a87cee73f2559ecebd4c539a58fdbf65edb3
agustinbaretto <agustin@Agustins-MacBook-Air.local>
2015-01-21 23:02:43 -0500

    personal contact

---
+++ application/views/pages/research.php
@@ -0,0 +1,23 @@
+<!-- +++++ Projects Section +++++ -->
+	
+	<div class="container pt">
+		<div class="row">
+			<div class="col-lg-6 col-lg-offset-3 centered">
+				<h3>RESEARCH</h3>
+				<hr>
+				<p>I am currently part of the <a href="http://cs.rochester.edu/hci" target="_blank">Human-Computer Interaction</a> Research Team at 
+				<a href="http://www.rochester.edu/">University of Rochester</a>. I have been working on many things like face recognition, voice 
+				recognition, prototyping techniques, ubiquitous computing to name some.</p>
+			</div>
+		</div>
+		<div class="row centered">	
+			<div class="col-lg-6">
+				<iframe width="560" height="315" src="//www.youtube.com/embed/cZk-6xNplUo?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+				<p>Paper prototyping project</p>
+			</div>
+			<div class="col-lg-6">
+				<iframe width="560" height="315" src="//www.youtube.com/embed/bBJpZWID2NY?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+				<p>Playing with Face Recognition libraries</p>
+			</div>
+		</div><!-- /row -->
+	</div><!-- /container -->
